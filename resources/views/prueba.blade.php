a367f4aae12abeefb5188221e31adcefb95c9d4f
manolo <viprestal@hotmail.com>
2021-10-15 19:55:41 +0200

    commit inicial

---
+++ resources/views/prueba.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="alert alert-success mt-5" role="alert">
+        Boostrap 5 is working using laravel 8 mix!
+    </div>
+</div>
+@endsection
